55822b0f52ee74750d177e4764e8b82b2022e59e
Raymond Zeng <raymondzeng@live.com>
2014-08-04 13:42:31 -0400

    naive shortest path by summing. google maps autocomplete

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,5 @@
 from topsort import topsort
+from itertools import izip
 
 def valid_schedules(tasks, deps):
     """
@@ -8,6 +9,7 @@ def valid_schedules(tasks, deps):
     """
     # each schedule is a list of task_ids
     schedules = topsort(tasks, deps)
+    result = []
     for schedule in schedules:
         time_now = 0
         # TODO : empty list?
@@ -21,19 +23,60 @@ def valid_schedules(tasks, deps):
 
             time_now = available + duration
         else:
-            print schedule
+            print "vs", schedule
+            result.append(schedule)
+    return result
             
+def shortest_path(tasks, dists, scheds):
+    """
+    tasks : dictionary of { id: (available, duration, deadline) }
+    dists : dictionary of { (from_id, to_id) : distance }
+    scheds : list of schedules where a schedule is a list of task ids
+    """
+    shortest_dist = -1
+    shortest = None
+    for sched in scheds:
+        dist = sum(get_dist(dists, a, b) for a, b in izip(sched, sched[1:]))
+        print "in", sched, dist
+        if shortest is None or dist < shortest_dist:
+            shortest_dist = dist
+            shortest = sched
+        
+    return (shortest_dist, shortest)
 
+def get_dist(dists, a, b):
+    """ 
+    dists is a dictionary where keys are tuples of (from_id, to_id) and the values are the distance from from_id to to_id. 
+
+    This function returns the value with key (a,b) or (b,a) since they are the same distance.
+
+    We need this function so we don't need to duplicate the dictionary with both keys (a,b) and (b,a)
+    
+    Will still throw an error if neither key exist
+    """
+    try:
+        return dists[(a,b)]
+    except KeyError:
+        return dists[(b,a)]
+        
 # <task_id> : (available, duraction, deadline)
 # the task can be done anytime between available and deadline
 tasks = { 1: (10, 2, 24),
           2: (11, 1, 14),
           3: (13, 1, 15),
-          4: (14, 4, 18)}
+          4: (14, 4, 18) }
+
+distances = { (1, 2) : 10,
+              (1, 3) : 12,
+              (1, 4) : 10,
+              (2, 3) : 5,
+              (2, 4) : 19,
+              (3, 4) : 16 }
 
 deps = { 1: [],
          2: [1],
          3: [],
-         4: [3]}
+         4: [3] }
 
-valid_schedules(tasks, deps)
+vs = valid_schedules(tasks, deps)
+print shortest_path(tasks, distances, vs)
--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,12 @@
+var map;
+var service;
+var searchBox;
+var markers = [];
+var MARKER_PATH = 'https://maps.gstatic.com/intl/en_us/mapfiles/marker_green';
+
 $(document).ready(function() {
-    $("#input_box").focus();
-    $('#input_box').keypress(function (e) {
+    $("#searchTextField").focus();
+    $('#searchTextField').keypress(function (e) {
         if (e.which == 13) {
             add_item($(this).val());
             e.preventDefault();
@@ -8,17 +14,91 @@ $(document).ready(function() {
     });
     
     $("#add_btn").click(function() {
-        add_item($("#input_box").val());
+        add_item($("#searchTextField").val());
     });
+
+    var pyrmont = new google.maps.LatLng(40.790278, -73.959722);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 13,
+        maxZoom: 18
+    });
+    
+    var request = {
+        location: pyrmont,
+        radius: '500',
+        query: 'restaurant'
+    };
+    service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, callback);
+    
+    var input = document.getElementById('searchTextField');
+    
+    searchBox = new google.maps.places.SearchBox(input);
+    
+    google.maps.event.addListener(searchBox, 'places_changed', onPlaceChanged);
 });
 
+function onPlaceChanged() {
+    var place = searchBox.getPlaces();
+    if (place[0].geometry) {
+        map.panTo(place[0].geometry.location);
+        var markerLetter = String.fromCharCode('A'.charCodeAt(0) + markers.length);
+        var markerIcon = MARKER_PATH + markerLetter + '.png';
+        
+        // Use marker animation to drop the icons incrementally on the map.
+        marker = new google.maps.Marker({
+            position: place[0].geometry.location,
+            animation: google.maps.Animation.DROP,
+            icon: markerIcon
+        });
+        
+        marker.place_id = place[0].place_id;
+        marker.hours = place[0].opening_hours;
+        marker.setMap(map);
+        
+        markers.push(marker);
+        console.log(markers);
+        zoomToFit();
+        // If the user clicks a hotel marker, show the details of that hotel
+        // in an info window.
+       // markers[i].placeResult = results[i];
+       // google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+       // setTimeout(dropMarker(i), i * 100);
+       // addResult(results[i], i);
+    }
+}
+
+function zoomToFit() {
+    // Create a new viewpoint bound
+    var bounds = new google.maps.LatLngBounds();
+    // Go through each...
+    for (var i = 0; i < markers.length; i++) {
+        var marker = markers[i];
+        // And increase the bounds to take this point
+        bounds.extend(marker.position);
+    }
+    // Fit these bounds to the map
+    map.fitBounds(bounds);
+}
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            console.log(place.geometry.location.lat());
+            //createMarker(results[i]);
+        }
+    }
+}
 
 function add_item(str) {
     $("#input_box").val("");
     task_str = '<span class="task_str">' + str + '</span>';
 
     btn = '<input type="button" class="button" value="X">';
-    html = $("<li>" + task_str + btn + "</li>");
+    html = $("<li>" + task_str + "</li>");
         
     $("#list").append(html);
     
