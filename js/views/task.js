5b425cb8dedef747c9901e8441eeb21e9a0454c4
Raymond Zeng <raymondzeng@live.com>
2014-08-27 12:17:11 -0400

    logic broken for blur and focus on edit box

--- js/views/task.js
+++ js/views/task.js
@@ -78,6 +78,7 @@ define([
         
         editTime: function(e) {
             console.log("edit");
+            consoel.log(e.target);
             var edit_box = $(e.target).siblings(".edit");
             edit_box.css("display", "block").focus().select();
             $(e.target).children(".edit_view").hide();
